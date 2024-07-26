1a1c11bd64e2c8d7eb9d4ea92a3ebbc3da3cce7a
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-07-26 18:48:38 +0530

    Day5 done

--- Day5/script.js
+++ Day5/script.js
@@ -30,3 +30,39 @@ const contain = (str) => {
     return str.includes("pandey");
 }
 console.log(contain("Saurabh"));
+
+// Function Parameters and Default Values
+function product( e, f=5){
+    return e*f;
+}
+console.log(product(4,7));
+
+function greeting(name,age=28){
+    const result = `Hello ${name}! I think your age is around ${age}.`;
+    return result;
+}
+console.log(greeting("Sanjeet"));
+
+// Higher order functions
+function order(fn,val){
+    for(let i=0; i<val; i++){
+        fn();
+    }
+}
+
+function sayHello(){
+    console.log("Hello World!");
+}
+order(sayHello,5);
+
+function multipleFunctions(func1,func2,value){
+    return func1(func2(value));
+}
+const multiplyBy5 = (val) => val*5;
+const add10 = (val) => val+10;
+
+const res = multipleFunctions(multiplyBy5,add10,5);
+console.log(res);
+
+
+
