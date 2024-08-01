b7d242d0719e132702a1959fc0cf60c41044bd44
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-08-01 23:51:00 +0530

    Day8 completed

--- Day8/script.js
+++ Day8/script.js
@@ -28,3 +28,46 @@ const {title,author} = book;
 console.log(`Title of the book: ${title}\n Author of the book: ${author}`);
 
 // Spread and Rest Operator
+// spread operator
+const exArr1 = [1,2,3];
+const exArr2 = [...exArr1,4,5,6,7];
+console.log(exArr2);
+// rest operator
+
+function sumOfN(...args){
+    return args.reduce((acc,curr)=> acc+curr,0);
+}
+console.log(sumOfN(1,2,3,4,5));
+
+// Default parameters
+function defaultParams(num1,num2=1){
+    return num1*num2;
+}
+console.log(defaultParams(5,9));
+console.log(defaultParams(8));
+
+// Enhanced Object Literals
+const identity = "Charlie";
+const umar = 83;
+
+const info = {
+    identity,
+    umar,
+    namaskara: function(){
+        console.log(`Hello, ${this.identity}. Is your age ${this.umar} real.`);
+    }
+}
+console.log(info);
+
+const propName1 = "name";
+const propName2 = "age";
+const propName3 = "country";
+
+const person = {
+  [propName1]: "Alice",
+  [propName2]: 30,
+  [propName3]: "USA"
+};
+
+console.log(person);
+
