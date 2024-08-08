52c69b8eb54427c3b283625a4ddbb1e74e45d0b0
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-08-08 17:34:35 +0530

    Day 11 successfully completed

--- Day11/script.js
+++ Day11/script.js
@@ -13,7 +13,7 @@ const greet = new Promise((resolve,reject)=>{
     else{
 
       setTimeout(()=>{
-        reject("something happened!");
+        reject("success rejected!");
       },4000);
     }
 })
@@ -48,5 +48,68 @@ const promiseChain = new Promise((resolve, reject) => {
     .catch((error) => {
       console.error(error);
     });
-  
 
+
+// Async/Await
+async function myFunction() {
+    let promise = new Promise((resolve, reject) => {
+      let retrieve = false;
+      if(retrieve){
+        setTimeout(() => {
+            resolve("retrieval successful!");
+        },1000);
+      }else{
+        setTimeout(() => {
+            reject("unable to fetch anything")
+        }, 500);
+      }
+    });
+  
+    try{
+        let result = await promise;
+        console.log(result);
+    }catch(error){
+        console.log(error);
+        
+    }
+  }
+  console.log(myFunction());
+
+  // Fetching Data from an API
+
+  async function fetchData(){
+    try{
+    let response = await fetch("https://jsonplaceholder.typicode.com/users/");
+    let data = await response.json();
+    console.log(data);
+    }
+    catch(err){console.log("fetch unsuccessful", err.message)}
+
+  }
+  console.log(fetchData());
+
+  //Concurrent Promises
+
+  async function fetchData2(){
+    let [res1, res2] = await Promise.all([
+        fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json()),
+        fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json())
+    ]);
+    console.log(`posts result: ${res1}\n todos results: ${res2}`);
+  }
+
+  let data = fetchData2();
+  console.log(data);
+
+// promise.race
+const promises = [
+    new Promise((resolve)=>setTimeout(()=>resolve("one"),100)),
+    new Promise((resolve)=> setTimeout(()=>resolve("two"),500))
+];
+
+Promise.race(promises)
+.then((val)=>console.log(val))
+.catch((err)=>{
+    console.log(err);
+})
+  
